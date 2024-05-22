abc5d92b7d9147096d26f47e834fb72b6cc5719b
brandaoconte <ferconte@gmail.com>
2024-05-22 19:54:49 -0400

    test2

--- src/test/java/br/ce/wcaquino/taskbackend/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/DateUtilsTest.java
@@ -1,6 +1,7 @@
 package br.ce.wcaquino.taskbackend;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
 
@@ -15,5 +16,11 @@ public class DateUtilsTest {
         LocalDate before = LocalDate.of(2000, 1, 1);
         boolean result = DateUtils.isEqualOrFutureDate(before);
         assertFalse(result);
+    }
+    @Test
+    public void isAfter() {
+        LocalDate before = LocalDate.of(2100, 1, 1);
+        boolean result = DateUtils.isEqualOrFutureDate(before);
+        assertTrue(result);
     }
 }
