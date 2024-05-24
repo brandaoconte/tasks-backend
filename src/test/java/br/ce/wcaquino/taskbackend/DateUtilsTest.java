fdf69e7e5964c66e3d26c74ed4ac6cc4c6f7812d
brandaoconte <ferconte@gmail.com>
2024-05-23 20:05:33 -0400

    Add test isEqual

--- src/test/java/br/ce/wcaquino/taskbackend/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/DateUtilsTest.java
@@ -23,4 +23,11 @@ public class DateUtilsTest {
         boolean result = DateUtils.isEqualOrFutureDate(before);
         assertTrue(result);
     }
+
+    @Test
+    public void isEqual() {
+        LocalDate before = LocalDate.now();
+        boolean result = DateUtils.isEqualOrFutureDate(before);
+        assertTrue(result);
+    }
 }
